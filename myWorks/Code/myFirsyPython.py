9aabc3451eb7562cd5cd3403d74833b56a177102
Sumly <xp55220012@gmail.com>
2021-12-16 10:43:34 +0700

    commit

--- myWorks/Code/myFirsyPython.py
+++ myWorks/Code/myFirsyPython.py
@@ -1,5 +1,16 @@
-import datetime
-now=datetime.datetime.now()
-print(now)
-aa ="Rub Phao Sop Phor Mueng"
-print(aa.lower())
+"""import math
+light = [int(i) for i in input("Enter n,m : ").split()]
+plan = [int(i) for i in input("Enter n,m : ").split()]
+man = int(input("Enter man number :  "))
+
+light_cost = 0
+for row in range(0, light[0]):
+        light_cost += sum ([int(i) for i in input("light: ").split()])
+
+oil_cost = plan[0] * plan[1] * man
+
+ticket_price = (light_cost + oil_cost) / man
+
+print(math.ceil(ticket_price))"""
+tp1 = (1,)
+print(type(tp1))
--- myWorks/Code/myFristPython.py
+++ myWorks/Code/myFristPython.py
@@ -1,1 +1,3 @@
-
+for i in range(1, 1001):
+    number = str(i)
+print(number)
